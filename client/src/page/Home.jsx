714c24205fbb86920c4fab4bddefda738541c8a8
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-28 11:33:31 +0900

    Adding CreateBattle Page

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { PageHOC } from '../components';
+import { PageHOC } from "../components";
 
 const Home = () => {
   return (
     <div>
-      
+      <h1 className="text-white text-xl">Hello From Home</h1>
     </div>
-  )
+  );
 };
 
 // export default Home;
 
 export default PageHOC(
   Home,
-  <>Welcome to Avax Gods <br /> a Web3 NFT Card Game</>,
-  <>Connect your wallet to start playing <br /> the ultimate Web3 Battle Card Game</>
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>
 );
